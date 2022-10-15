24cd462524162360d99e31e2866c66de3eab99e3
David Asensio Cañas <david.asensio@icloud.com>
2022-10-15 21:21:24 +0200

    Add ZH-L16C compartment data

---
+++ src/buhlmann/ZH-L16C.js
@@ -0,0 +1,87 @@
+const compartments = [
+  {
+    name: '1',
+    nitrogen: { half_time: 4.0, a: 1.2599, b: 0.5240 },
+    helium: { half_time: 1.51, a: 1.7424, b: 0.4245 }
+  },
+  {
+    name: '1b',
+    nitrogen: { half_time: 5.0, a: 1.1696, b: 0.5578 },
+    helium: { half_time: 1.88, a: 1.6189, b: 0.4770 }
+  },
+  {
+    name: '2',
+    nitrogen: { half_time: 8.0, a: 1.0000, b: 0.6514 },
+    helium: { half_time: 3.02, a: 1.3830, b: 0.5747 }
+  },
+  {
+    name: '3',
+    nitrogen: { half_time: 12.5, a: 0.8618, b: 0.7222 },
+    helium: { half_time: 4.72, a: 1.1919, b: 0.6527 }
+  },
+  {
+    name: '4',
+    nitrogen: { half_time: 18.5, a: 0.7562, b: 0.7825 },
+    helium: { half_time: 6.99, a: 1.0458, b: 0.7223 }
+  },
+  {
+    name: '5',
+    nitrogen: { half_time: 27.0, a: 0.6491, b: 0.8126 },
+    helium: { half_time: 10.21, a: 0.9220, b: 0.7582 }
+  },
+  {
+    name: '6',
+    nitrogen: { half_time: 38.3, a: 0.5316, b: 0.8434 },
+    helium: { half_time: 14.48, a: 0.8205, b: 0.7957 }
+  },
+  {
+    name: '7',
+    nitrogen: { half_time: 54.3, a: 0.4681, b: 0.8693 },
+    helium: { half_time: 20.53, a: 0.7305, b: 0.8279 }
+  },
+  {
+    name: '8',
+    nitrogen: { half_time: 77.0, a: 0.4301, b: 0.8910 },
+    helium: { half_time: 29.11, a: 0.6502, b: 0.8553 }
+  },
+  {
+    name: '9',
+    nitrogen: { half_time: 109.0, a: 0.4049, b: 0.9092 },
+    helium: { half_time: 41.20, a: 0.5950, b: 0.8757 }
+  },
+  {
+    name: '10',
+    nitrogen: { half_time: 146.0, a: 0.3719, b: 0.9222 },
+    helium: { half_time: 55.19, a: 0.5545, b: 0.8903 }
+  },
+  {
+    name: '11',
+    nitrogen: { half_time: 187.0, a: 0.3447, b: 0.9319 },
+    helium: { half_time: 70.69, a: 0.5333, b: 0.8997 }
+  },
+  {
+    name: '12',
+    nitrogen: { half_time: 239.0, a: 0.3176, b: 0.9403 },
+    helium: { half_time: 90.34, a: 0.5189, b: 0.9073 }
+  },
+  {
+    name: '13',
+    nitrogen: { half_time: 305.0, a: 0.2828, b: 0.9477 },
+    helium: { half_time: 115.29, a: 0.5181, b: 0.9122 }
+  },
+  {
+    name: '14',
+    nitrogen: { half_time: 390.0, a: 0.2716, b: 0.9544 },
+    helium: { half_time: 147.42, a: 0.5176, b: 0.9171 }
+  },
+  {
+    name: '15',
+    nitrogen: { half_time: 498.0, a: 0.2523, b: 0.9602 },
+    helium: { half_time: 188.24, a: 0.5172, b: 0.9217 }
+  },
+  {
+    name: '16',
+    nitrogen: { half_time: 635.0, a: 0.2327, b: 0.9653 },
+    helium: { half_time: 240.03, a: 0.5119, b: 0.9267 }
+  }
+]
