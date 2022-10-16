c19e9272b3a5b38667fbf9fd7f4ba9d3c958319a
David Asensio Cañas <david.asensio@icloud.com>
2022-10-16 15:00:59 +0200

    Split dive mapper into smaller parts using ranmda and FP patterns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { map, pipe } from 'ramda'
 
 import { DiveProfileChart } from './components'
 
@@ -12,51 +13,75 @@ const Wrapper = styled.main`
   overflow: hidden;
 `
 
-const fromDepthToPressure = depth => {
+const calculateAbmientPressure = data_point => {
+  const { depth } = data_point
+
   const surface_pressure_in_bars = 1
-  // const fresh_water_density = 997.0474 
+  // const fresh_water_density = 997.0474
   const salt_water_density = 1023.6 // kg/m^3 (in salty water) TODO: account for temperature?
   const gravity = 9.8 // m*s^2
-  const pressure_in_pascals = depth*salt_water_density*gravity
-  const pressure_in_bars = pressure_in_pascals/100000
+  const pressure_in_pascals = depth * salt_water_density * gravity
+  const pressure_in_bars = pressure_in_pascals / 100000
 
-  return pressure_in_bars + surface_pressure_in_bars
+  return {
+    ...data_point,
+    pressure: pressure_in_bars + surface_pressure_in_bars,
+  }
 }
 
-const ppN2 = (ambient_pressure, nitrogen_percentage) => {
+const { gas_mixtures } = dive
+
+const calculatePartialPressureO2 = (data_point) => {
+  const { pressure } = data_point
+
+  return {
+    ...data_point,
+    pressureO2: pressure * gas_mixtures.oxygen,
+  }
+}
+
+const calculatePartialPressureN2 = (data_point) => {
+  const { pressure } = data_point
+
   const surface_pressure_in_bars = 1
   const water_vapour_partial_pressure = 0.0567 // TODO: Validate this constant, should it change with depth?
 
-  return ambient_pressure * nitrogen_percentage * (surface_pressure_in_bars - water_vapour_partial_pressure)
+  return {
+    ...data_point,
+    pressureN:
+      pressure *
+      gas_mixtures.nitrogen *
+      (surface_pressure_in_bars - water_vapour_partial_pressure),
+  }
 }
 
-const tranformDiveIntoChartData = ({ samples, gas_mixtures }) => samples.map(
-  sample => {
-    const { depth, time } = sample
-    const pressure = fromDepthToPressure(depth)
-    const pressureO2 = pressure * gas_mixtures.oxygen
-    const pressureN = ppN2(pressure, gas_mixtures.nitrogen)
+const calculateChartAxis = (data_point) => {
+  const { time, depth } = data_point
 
-    return {
-      depth,
-      time,
-      pressure,
-      pressureO2,
-      pressureN,
-      x: time,
-      y: depth
-    }
+  return {
+    ...data_point,
+    x: time,
+    y: depth,
   }
+}
+
+const tranformDiveSamplesIntoChartData = map(
+  pipe(
+    calculateAbmientPressure,
+    calculatePartialPressureO2,
+    calculatePartialPressureN2,
+    calculateChartAxis,
+  ),
 )
 
-const data = tranformDiveIntoChartData(dive)
+const data = tranformDiveSamplesIntoChartData(dive.samples)
 
 function App() {
   return (
     <Wrapper>
       <DiveProfileChart data={data} />
     </Wrapper>
-  );
+  )
 }
 
-export default App;
+export default App
