b5aab8fdd55050f27ce2352562568d270973655b
David Asensio Cañas <david.asensio@icloud.com>
2022-10-17 15:11:45 +0200

    Formatting

--- src/components/CompartmentsGasChart.jsx
+++ src/components/CompartmentsGasChart.jsx
@@ -8,52 +8,54 @@ const Wrapper = styled.div`
   overflow-y: scroll;
 `
 
-export const CompartmentsGasChart = ({ data, ambientPressure, maxAmbientPressure }) => (
+export const CompartmentsGasChart = ({
+  data,
+  ambientPressure,
+  maxAmbientPressure,
+}) => (
   <Wrapper>
     <ResponsiveBar
-        data={data}
-        keys={[
-            'gas_pressure'
-        ]}
-        indexBy="id"
-        margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
-        padding={0.3}
-        layout="horizontal"
-        valueFormat=">-.2f"
-        label={({ formattedValue }) => `${formattedValue} bar`}
-        minValue={0}
-        maxValue={maxAmbientPressure}
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={{ scheme: 'blues' }}
-        tooltip={false}
-        markers={[
-            {
-                axis: 'x',
-                value: ambientPressure,
-                lineStyle: { stroke: 'rgba(0, 0, 0, .35)', strokeWidth: 2 },
-                legend: `Ambient Pressure (${ambientPressure.toFixed(2)} bar)`,
-                legendOrientation: 'vertical',
-            },
-        ]}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Tissue Pressure Load',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Compartment',
-            legendPosition: 'middle',
-            legendOffset: -40
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
+      data={data}
+      keys={['gas_pressure']}
+      indexBy="id"
+      margin={{ top: 50, right: 60, bottom: 50, left: 60 }}
+      padding={0.3}
+      layout="horizontal"
+      valueFormat=">-.2f"
+      label={({ formattedValue }) => `${formattedValue} bar`}
+      minValue={0}
+      maxValue={maxAmbientPressure}
+      valueScale={{ type: 'linear' }}
+      indexScale={{ type: 'band', round: true }}
+      colors={{ scheme: 'blues' }}
+      tooltip={false}
+      markers={[
+        {
+          axis: 'x',
+          value: ambientPressure,
+          lineStyle: { stroke: 'rgba(0, 0, 0, .35)', strokeWidth: 2 },
+          legend: `Ambient Pressure (${ambientPressure.toFixed(2)} bar)`,
+          legendOrientation: 'vertical',
+        },
+      ]}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Tissue Pressure Load',
+        legendPosition: 'middle',
+        legendOffset: 32,
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Compartment',
+        legendPosition: 'middle',
+        legendOffset: -40,
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
     />
   </Wrapper>
 )
