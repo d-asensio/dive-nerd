95ded973e4df5bbf172d5da1dfe1978187ac9031
David Asensio Cañas <david.asensio@icloud.com>
2022-10-28 01:00:56 +0200

    Add more dives

--- src/sections/DiveLog.jsx
+++ src/sections/DiveLog.jsx
@@ -17,6 +17,34 @@ export const Divelog = ({ gridArea }) => {
           depth='48.2 m'
           time='00:38'
         />
+        <DiveList.Divider />
+        <DiveList.Item
+          name='Thomas Reef'
+          date='25/10/2022 10:30 AM'
+          depth='48.2 m'
+          time='00:38'
+        />
+        <DiveList.Divider />
+        <DiveList.Item
+          name='Thomas Reef'
+          date='25/10/2022 10:30 AM'
+          depth='48.2 m'
+          time='00:38'
+        />
+        <DiveList.Divider />
+        <DiveList.Item
+          name='Thomas Reef'
+          date='25/10/2022 10:30 AM'
+          depth='48.2 m'
+          time='00:38'
+        />
+        <DiveList.Divider />
+        <DiveList.Item
+          name='Thomas Reef'
+          date='25/10/2022 10:30 AM'
+          depth='48.2 m'
+          time='00:38'
+        />
       </DiveList>
     </DiveLogPanel>
   )
