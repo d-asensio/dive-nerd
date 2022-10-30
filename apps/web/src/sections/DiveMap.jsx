820a1ccc147c302e766a4a970799ad3705de4b3f
David Asensio Cañas <david.asensio@icloud.com>
2022-10-30 10:53:06 +0100

    Add tooltip and navigation to markers

--- apps/web/src/sections/DiveMap.jsx
+++ apps/web/src/sections/DiveMap.jsx
@@ -4,46 +4,68 @@ import Map, { Marker } from 'react-map-gl'
 import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
 
 import Badge from '@mui/joy/Badge'
+import Tooltip from '@mui/joy/Tooltip'
+
 import { useSelector } from '../store'
 import { diveSelector, diveIdListSelector } from '../entities'
+import {useNavigate} from 'react-router-dom';
 
 const DiveMapMarker = ({ diveId }) => {
-  const { geographicCoordinates: { latitude, longitude } } = useSelector((state) =>
+  const navigate = useNavigate()
+
+  const handleNavigate = useCallback(() => navigate(`/dive/${diveId}`), [diveId])
+
+  const { name, geographicCoordinates: { latitude, longitude } } = useSelector((state) =>
     diveSelector(state, diveId),
   )
 
   return (
     <Marker longitude={longitude} latitude={latitude} anchor="center">
-      <Badge
-        badgeInset="14%"
-        color="success"
-        sx={{
-          '& .JoyBadge-badge': {
-            '&::after': {
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              borderRadius: '50%',
-              animation: 'ripple 1.2s infinite ease-in-out',
-              border: '2px solid',
-              borderColor: 'success.500',
-              content: '""',
+      <Tooltip
+        title={name}
+        placement="top"
+        variant="outlined"
+        arrow
+      >
+        <Badge
+          badgeInset="50%"
+          color="success"
+          size="lg"
+          sx={{
+            p: 1.5,
+            transition: 'transform 300ms',
+            cursor: 'pointer',
+            '&:hover': {
+              transform: 'scale(1.5)'
             },
-          },
-          '@keyframes ripple': {
-            '0%': {
-              transform: 'scale(1)',
-              opacity: 1,
+            '& .JoyBadge-badge': {
+              '&::after': {
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                borderRadius: '50%',
+                animation: 'ripple 1.2s infinite ease-in-out',
+                border: '2px solid',
+                borderColor: 'success.500',
+                content: '""',
+              },
             },
-            '100%': {
-              transform: 'scale(2)',
-              opacity: 0,
+            '@keyframes ripple': {
+              '0%': {
+                transform: 'scale(1)',
+                opacity: 1,
+              },
+              '100%': {
+                transform: 'scale(2)',
+                opacity: 0,
+              },
             },
-          },
-        }}
-      />
+          }}
+          onClick={handleNavigate}
+        />
+      </Tooltip>
     </Marker>
   )
 }
