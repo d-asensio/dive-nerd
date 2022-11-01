1fe38ea37db3e8039d6fad0aff61b8459cf10897
David Asensio Cañas <david.asensio@icloud.com>
2022-11-01 22:15:45 +0100

    Move fields into sections

--- apps/web/src/components/DiveInfo.jsx
+++ apps/web/src/components/DiveInfo.jsx
@@ -32,6 +32,22 @@ function FieldsRow ({ children, columns }) {
   </Box>
 }
 
+function Section ({ title, children }) {
+  return <Box sx={{ width: '100%' }}>
+    <Divider sx={{ '--Divider-childPosition': `0%` }}>
+      <Typography level="h5">{title}</Typography>
+    </Divider>
+    <Box
+      sx={{
+        p: 2,
+        width: '100%'
+      }}
+    >
+      {children}
+    </Box>
+  </Box>
+}
+
 export const DiveInfo = () => {
   const [date, setDate] = useState(dayjs('2022-04-07'))
 
@@ -71,7 +87,7 @@ export const DiveInfo = () => {
                 }
               >
                 <IconButton variant="plain">
-                  <CalendarMonthIcon />
+                  <CalendarMonthIcon/>
                 </IconButton>
               </Tooltip>
             }
@@ -100,137 +116,132 @@ export const DiveInfo = () => {
             Ice
           </Choice>
         </MultipleChoiceField>
-        <Divider sx={{ '--Divider-childPosition': `0%` }}>
-          <Typography level='h5'>Time</Typography>
-        </Divider>
-        <FieldsRow>
+        <Section title='Time'>
+          <FieldsRow>
+            <TextField
+              sx={{ minWidth: 0 }}
+              label="Total duration"
+              size="lg"
+              value="60:00"
+              endDecorator="mins"
+            />
+            <TextField
+              sx={{ minWidth: 0 }}
+              label="Bottom time"
+              size="lg"
+              value="12:00"
+              endDecorator="mins"
+            />
+          </FieldsRow>
+          <FieldsRow>
+            <TextField
+              label="Start time"
+              size="lg"
+              value="12:45 PM"
+            />
+            <TextField
+              label="End time"
+              size="lg"
+              value="01:45 PM"
+            />
+          </FieldsRow>
+        </Section>
+        <Section title='Depth'>
+          <FieldsRow>
+            <TextField
+              sx={{ minWidth: 0 }}
+              label="Maximum Depth"
+              size="lg"
+              value="35.4"
+              endDecorator="meters"
+            />
+            <TextField
+              sx={{ minWidth: 0 }}
+              label="Average Depth"
+              size="lg"
+              value="10.4"
+              endDecorator="meters"
+            />
+          </FieldsRow>
+        </Section>
+        <Section title='Water'>
+          <FieldsRow>
+            <TextField
+              sx={{ minWidth: 0 }}
+              label="Average temperature"
+              size="lg"
+              value="23"
+              endDecorator="ºC"
+            />
+            <SelectField
+              sx={{ minWidth: '100%' }}
+              label="Salinity"
+              size="lg"
+              defaultValue="sea"
+            >
+              <Option value="fresh">Fresh (1.00 kg/l)</Option>
+              <Option value="brackish">Brackish (1.02 kg/l)</Option>
+              <Option value="sea">Sea (1.03 kg/l)</Option>
+            </SelectField>
+            <SelectField
+              sx={{ minWidth: '100%' }}
+              label="Visibility"
+              size="lg"
+              defaultValue="good"
+            >
+              <Option value="good">Good (+20m)</Option>
+              <Option value="medium">Medium (10-20m)</Option>
+              <Option value="bad">Bad (-5m)</Option>
+            </SelectField>
+          </FieldsRow>
+        </Section>
+        <Section title='Gear'>
           <TextField
-            sx={{ minWidth: 0 }}
-            label="Total duration"
+            label="Weights"
             size="lg"
-            value="60:00"
-            endDecorator="mins"
+            value="6"
+            endDecorator="kg"
           />
+        </Section>
+        <Section title='Gas'>
+          <FieldsRow>
+            <SelectField
+              sx={{ minWidth: '100%' }}
+              label="Tank volume"
+              size="lg"
+              defaultValue="10-liters"
+            >
+              <Option value="7-liters">7 liters</Option>
+              <Option value="10-liters">10 liters</Option>
+              <Option value="12-liters">12 liters</Option>
+              <Option value="15-liters">15 liters</Option>
+            </SelectField>
+            <GasMixtureField
+              defaultValue={21}
+            />
+          </FieldsRow>
+          <FieldsRow>
+            <TextField
+              label="Start tank pressure"
+              size="lg"
+              value="200"
+              endDecorator="bar"
+            />
+            <TextField
+              label="End tank pressure"
+              size="lg"
+              value="50"
+              endDecorator="bar"
+            />
+          </FieldsRow>
           <TextField
-            sx={{ minWidth: 0 }}
-            label="Bottom time"
+            label="SAC Rate"
             size="lg"
-            value="12:00"
-            endDecorator="mins"
+            value="12"
+            endDecorator="l/min"
+            readOnly
           />
-        </FieldsRow>
-        <FieldsRow>
-          <TextField
-            label="Start time"
-            size="lg"
-            value="12:45 PM"
-          />
-          <TextField
-            label="End time"
-            size="lg"
-            value="01:45 PM"
-          />
-        </FieldsRow>
-        <Divider sx={{ '--Divider-childPosition': `0%` }}>
-          <Typography level='h5'>Depth</Typography>
-        </Divider>
-        <FieldsRow>
-          <TextField
-            sx={{ minWidth: 0 }}
-            label="Maximum Depth"
-            size="lg"
-            value="35.4"
-            endDecorator="meters"
-          />
-          <TextField
-            sx={{ minWidth: 0 }}
-            label="Average Depth"
-            size="lg"
-            value="10.4"
-            endDecorator="meters"
-          />
-        </FieldsRow>
-        <Divider sx={{ '--Divider-childPosition': `0%` }}>
-          <Typography level='h5'>Water</Typography>
-        </Divider>
-        <FieldsRow>
-          <TextField
-            sx={{ minWidth: 0 }}
-            label="Average temperature"
-            size="lg"
-            value="23"
-            endDecorator="ºC"
-          />
-          <SelectField
-            sx={{ minWidth: '100%' }}
-            label="Salinity"
-            size="lg"
-            defaultValue="sea"
-          >
-            <Option value="fresh">Fresh (1.00 kg/l)</Option>
-            <Option value="brackish">Brackish (1.02 kg/l)</Option>
-            <Option value="sea">Sea (1.03 kg/l)</Option>
-          </SelectField>
-          <SelectField
-            sx={{ minWidth: '100%' }}
-            label="Visibility"
-            size="lg"
-            defaultValue="good"
-          >
-            <Option value="good">Good (+20m)</Option>
-            <Option value="medium">Medium (10-20m)</Option>
-            <Option value="bad">Bad (-5m)</Option>
-          </SelectField>
-        </FieldsRow>
-        <Divider sx={{ '--Divider-childPosition': `0%` }}>
-          <Typography level='h5'>Gear</Typography>
-        </Divider>
-        <TextField
-          label="Weights"
-          size="lg"
-          value="6"
-          endDecorator="kg"
-        />
-        <Divider sx={{ '--Divider-childPosition': `0%` }}>
-          <Typography level='h5'>Gas</Typography>
-        </Divider>
-        <FieldsRow>
-          <SelectField
-            sx={{ minWidth: '100%' }}
-            label="Tank volume"
-            size="lg"
-            defaultValue="10-liters"
-          >
-            <Option value="7-liters">7 liters</Option>
-            <Option value="10-liters">10 liters</Option>
-            <Option value="12-liters">12 liters</Option>
-            <Option value="15-liters">15 liters</Option>
-          </SelectField>
-          <GasMixtureField
-            defaultValue={21}
-          />
-        </FieldsRow>
-        <FieldsRow>
-          <TextField
-            label="Start tank pressure"
-            size="lg"
-            value="200"
-            endDecorator="bar"
-          />
-          <TextField
-            label="End tank pressure"
-            size="lg"
-            value="50"
-            endDecorator="bar"
-          />
-        </FieldsRow>
-        <TextField
-          label="SAC Rate"
-          size="lg"
-          value="12"
-          endDecorator="l/min"
-          readOnly
-        />
+        </Section>
       </Box>
     </LocalizationProvider>
   )
