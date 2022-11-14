2f9aa5483af630840fe74dee74bb9d2647abd62d
David Asensio Cañas <david.asensio@icloud.com>
2022-11-14 20:07:02 +0100

    Add documentation for alveolarInertGasPartialPressure function

--- packages/dive-physics/src/refactor/index.js
+++ packages/dive-physics/src/refactor/index.js
@@ -1,3 +1,17 @@
+/**
+ * Calculates the alveolar partial pressure of an inert gas. This is the
+ * inspired partial pressure of the gas minus the water vapour pressure in the
+ * lungs, which is considered constant.
+ *
+ * This function can be used with any pressure units (pascal, mm Hg, bar, msw,
+ * fsw), the only requirement is to be consistent in all the provided pressure
+ * parameters.
+ *
+ * @param Pa
+ * @param Pwv
+ * @param Fig
+ * @returns {number}
+ */
 export const alveolarInertGasPartialPressure = ({
   ambientPressure: Pa,
   waterVaporPressure: Pwv,
