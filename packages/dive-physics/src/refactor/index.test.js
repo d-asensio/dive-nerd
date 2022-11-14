afb399b8442fc25a7e4968a237ec5cd0745a0418
David Asensio Cañas <david.asensio@icloud.com>
2022-11-14 20:17:37 +0100

    Add tests with other pressure units and depths to alveolarInertGasPartialPressure function

--- packages/dive-physics/src/refactor/index.test.js
+++ packages/dive-physics/src/refactor/index.test.js
@@ -3,14 +3,46 @@ import { alveolarInertGasPartialPressure } from './index'
 describe('alveolarInertGasPartialPressure', () => {
   it.each([
     {
-      labelDepth: 'the surface',
+      labelDepth: 'the surface of the sea',
       labelUnits: 'fsw',
       ambientPressure: 33,
       waterVaporPressure: 2.042,
       inertGasFraction: 0.4,
       expectedResult: 12.3832
+    },
+    {
+      labelDepth: 'the surface of the sea',
+      labelUnits: 'msw',
+      ambientPressure: 10,
+      waterVaporPressure: 0.627,
+      inertGasFraction: 0.4,
+      expectedResult: 3.7492
+    },
+    {
+      labelDepth: 'the surface of the sea',
+      labelUnits: 'msw',
+      ambientPressure: 10,
+      waterVaporPressure: 0.627,
+      inertGasFraction: 0.79,
+      expectedResult: 7.4046
+    },
+    {
+      labelDepth: '10 meters from sea surface',
+      labelUnits: 'msw',
+      ambientPressure: 20,
+      waterVaporPressure: 0.627,
+      inertGasFraction: 0.79,
+      expectedResult: 15.3046
+    },
+    {
+      labelDepth: '10 meters from sea surface',
+      labelUnits: 'bar',
+      ambientPressure: 2,
+      waterVaporPressure: 0.0627,
+      inertGasFraction: 0.79,
+      expectedResult: 1.5304
     }
-  ])('should be $expectedResult $labelUnits for $inertGasFraction fraction of inert gas at $labelUnits', ({
+  ])('should be $expectedResult $labelUnits for $inertGasFraction fraction of inert gas at $labelDepth', ({
     ambientPressure,
     waterVaporPressure,
     inertGasFraction,
@@ -22,6 +54,6 @@ describe('alveolarInertGasPartialPressure', () => {
       inertGasFraction
     })
 
-    expect(result).toBe(expectedResult)
+    expect(result).toBeCloseTo(expectedResult, 3)
   })
 })
