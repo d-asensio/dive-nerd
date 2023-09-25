9113e8898cb354d0ef907c06ecc7a1155d0d21b5
David Asensio Cañas <david.asensio@icloud.com>
2023-09-25 19:17:34 +0200

    Add top bar

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,30 @@
-import {Button} from "@/components/ui/button";
+import {Separator} from "@/components/ui/separator";
+
+const TopBar = () => (
+  <div className="hidden h-full flex-col md:flex">
+    <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+      <div className="flex items-center space-x-2">
+        <span className="text-4xl">
+          🤿
+        </span>
+        <h2 className="text-2xl font-semibold">
+          DiveNerd
+        </h2>
+      </div>
+      <div className="ml-auto flex w-full space-x-2 sm:justify-end">
+        <div className="hidden space-x-2 md:flex">
+
+        </div>
+      </div>
+    </div>
+    <Separator />
+  </div>
+)
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-        Dive
-      </Button>
+    <main>
+      <TopBar />
     </main>
   )
 }
