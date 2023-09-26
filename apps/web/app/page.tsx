5928193afafb6359f5c43c052b5323a3fd8a8c13
David Asensio Cañas <david.asensio@icloud.com>
2023-09-26 09:56:12 +0200

    Add profile table

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,17 @@
-import {Separator} from "@/components/ui/separator";
+import * as React from "react";
+import { Separator } from "@/components/ui/separator";
 import { ShareButton } from "@/components/app/share-button";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Input, InputProps } from "@/components/ui/input";
+import {cn} from "@/lib/utils";
 
 const TopBar = () => (
   <div className="hidden h-full flex-col md:flex">
@@ -22,10 +34,93 @@ const TopBar = () => (
   </div>
 )
 
+interface InputWithUnitsProps extends Omit<InputProps, "decorator"> {
+  units: string
+}
+
+const InputWithUnits = ({ units, ...props }: InputWithUnitsProps) => (
+  <Input
+    decorator={
+      <>
+        <Separator orientation={"vertical"} />
+        <span
+          className={cn(
+            "flex p-2 items-center w-auto border-l-0 rounded-l-none text-muted-foreground select-none whitespace-nowrap"
+          )}
+        >
+          {units}
+        </span>
+      </>
+    }
+    {...props}
+  />
+)
+
+export function ProfileTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>
+            Depth
+          </TableHead>
+          <TableHead>
+            Duration
+          </TableHead>
+          <TableHead className="w-[300px]">
+            Gas
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">
+            <InputWithUnits
+              size="sm"
+              units='meters'
+              type="number"
+              defaultValue="30"
+            />
+          </TableCell>
+          <TableCell>
+            <InputWithUnits
+              size="sm"
+              units='minutes'
+              type="number"
+              defaultValue="30"
+            />
+          </TableCell>
+          <TableCell>
+            <div className='flex items-center space-x-2'>
+              <InputWithUnits
+                size="sm"
+                units='% O2'
+                type="number"
+                defaultValue="30"
+              />
+              <span>/</span>
+              <InputWithUnits
+                size="sm"
+                units='% He'
+                type="number"
+                defaultValue="30"
+              />
+            </div>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
+
+
 export default function Home() {
   return (
     <main>
       <TopBar />
+      <div className="container p-6">
+        <ProfileTable />
+      </div>
     </main>
   )
 }
--- apps/web/components/ui/input.tsx
+++ apps/web/components/ui/input.tsx
@@ -1,25 +1,88 @@
+"use client"
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import {cva, VariantProps} from "class-variance-authority";
+
+interface UseIsFocusedResult  {
+  isFocused: boolean
+  inputProps: React.InputHTMLAttributes<HTMLInputElement>
+}
+
+const useIsFocused = (props: React.InputHTMLAttributes<HTMLInputElement>): UseIsFocusedResult => {
+  const [isFocused, setIsFocused] = React.useState(false)
+  const onFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    props?.onFocus?.(e)
+    setIsFocused(true)
+  }
+
+  const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    props?.onBlur?.(e)
+    setIsFocused(false)
+  }
+
+
+  return {
+    isFocused,
+    inputProps: {
+      ...props,
+      onFocus,
+      onBlur
+    }
+  }
+}
+
+const inputVariants = cva(
+  "flex w-full rounded-md border border-input bg-background text-sm ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      size: {
+        default: "h-10",
+        sm: "h-8"
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+)
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size">,
+    VariantProps<typeof inputVariants> {
+      decorator: React.ReactElement | string
+    }
+
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, size, decorator, ...props }, ref) => {
+    const {isFocused, inputProps} = useIsFocused(props)
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      <label
+        className={
+          cn(
+            inputVariants({ size, className }),
+            "flex items-center",
+            isFocused && "outline-none ring-2 ring-ring ring-offset-2"
+          )
+        }
+      >
+        <div className='flex-1 px-3 py-2'>
+          <input
+            className="w-full outline-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground"
+            type={type}
+            ref={ref}
+            {...inputProps}
+          />
+        </div>
+        {decorator}
+      </label>
     )
   }
 )
+
 Input.displayName = "Input"
 
 export { Input }
