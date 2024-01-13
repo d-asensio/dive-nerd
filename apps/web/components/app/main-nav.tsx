dd99bb5cd42c3905894b5eb417e1afa398377b47
David Asensio Cañas <david.asensio@icloud.com>
2024-01-13 19:00:40 +0100

    add menu

---
+++ apps/web/components/app/main-nav.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import {cn} from "@/lib/utils";
+import Link from "next/link";
+
+export const MainNav = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => {
+  return (
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      {...props}
+    >
+      <Link
+        href="/"
+        className="text-sm whitespace-nowrap font-medium transition-colors hover:text-primary"
+      >
+        Planner
+      </Link>
+      <Link
+        href="/learn/gas-planning-guide"
+        className="text-sm whitespace-nowrap font-medium transition-colors hover:text-primary"
+      >
+        Learn
+      </Link>
+    </nav>
+  )
+}
--- apps/web/components/app/top-bar.tsx
+++ apps/web/components/app/top-bar.tsx
@@ -3,11 +3,12 @@ import * as React from "react";
 import {ShareButton} from "@/components/app/share-button";
 import {LanguageSelector} from "@/components/app/language-selector";
 import {Separator} from "@/components/ui/separator";
+import {MainNav} from "@/components/app/main-nav";
 
 export const TopBar = () => (
   <div className="hidden h-full flex-col md:flex">
     <div
-      className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
+      className="container flex flex-col items-start justify-between space-y-2 space-x-8 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
       <div className="flex items-center space-x-2">
         <span className="text-4xl">
           🤿
@@ -16,6 +17,7 @@ export const TopBar = () => (
           DiveNerd
         </h2>
       </div>
+      <MainNav/>
       <div className="ml-auto flex w-full space-x-2 sm:justify-end">
         <div className="hidden space-x-2 md:flex">
           <ShareButton/>
