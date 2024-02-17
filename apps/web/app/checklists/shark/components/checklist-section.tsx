8d9723dd2e17e8f638fb8069e03f341ca3ae0a28
David Asensio Cañas <david.asensio@icloud.com>
2024-02-17 14:40:27 +0100

    add sticky behaviour to checklist sections

--- apps/web/app/checklists/shark/components/checklist-section.tsx
+++ apps/web/app/checklists/shark/components/checklist-section.tsx
@@ -9,20 +9,22 @@ type ChecklistSectionProps = PropsWithChildren<{
 
 export function ChecklistSection({ title, subtitle, children }: ChecklistSectionProps) {
   return (
-    <div>
-      <Separator className="my-4"/>
-      <div className="space-y-1">
-        <h4 className="text-sm font-medium leading-none">
-          {title}
-        </h4>
-        <p className="text-sm text-muted-foreground">
-          {subtitle}
-        </p>
+    <>
+      <div className="sticky top-0 bg-background z-10">
+        <Separator className="my-4"/>
+        <div className="space-y-1">
+          <h4 className="text-sm font-medium leading-none">
+            {title}
+          </h4>
+          <p className="text-sm text-muted-foreground">
+            {subtitle}
+          </p>
+        </div>
+        <Separator className="my-4"/>
       </div>
-      <Separator className="my-4"/>
       <div className="p-4 space-y-6">
         {children}
       </div>
-    </div>
+    </>
   );
 }
